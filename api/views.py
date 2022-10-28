57a5ea1bdca11f8f52f56fd3002a3029c4df8304
Alao Kolapo <alaokolapo7@gmail.com>
2022-10-28 09:37:02 +0100

    init

---
+++ api/views.py
@@ -0,0 +1,9 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+@api_view(['GET'])
+def getData(request):
+    data = {"slackUsername": "Alao Kolapo", 
+     "backend": True, "age": 21, 
+     "bio": "I am an up and coming Django backend developer."}
+    return Response(data)
