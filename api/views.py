365fc77b9d1d840b4c539c1ee0b418673ebe8368
Alao Kolapo <alaokolapo7@gmail.com>
2022-10-29 09:08:14 +0100

    updated username

--- api/views.py
+++ api/views.py
@@ -3,7 +3,7 @@ from rest_framework.decorators import api_view
 
 @api_view(['GET'])
 def getData(request):
-    data = {"slackUsername": "Alao Kolapo", 
+    data = {"slackUsername": "alaokolapo7", 
      "backend": True, "age": 21, 
      "bio": "I am an up and coming Django backend developer."}
     return Response(data)
